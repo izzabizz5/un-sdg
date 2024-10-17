bcea5ab3579b8af3a8cbe432de2b21dff2cd6216
izzabizz5 <154451126+izzabizz5@users.noreply.github.com>
2024-10-17 12:31:16 -0400

    change it again

--- rollup.config.js
+++ rollup.config.js
@@ -24,7 +24,7 @@ export default {
     /** Resolve bare module imports */
     copy({
       targets: [
-        { src: 'src/lib/', dest: 'public' }
+        { src: 'lib/', dest: 'public' }
       ]
     }),
     nodeResolve(),
